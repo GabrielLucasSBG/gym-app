ab3fcb8446aae36f42ec0f7e390be5769aaefe9b
GabrielLucasSBG <gabrielbenfica@hotmail.com>
2021-08-09 14:14:30 -0300

    initial commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'name' => 'Gabriel',
+            'email' => 'gabriel@email.com'
+        ];
+        return Inertia::render('Dashboard/Index', $data);
+    }
+}
